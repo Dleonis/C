50b845a7e84214458297e5a174feb45304debf7b
Leonis <1005728047@qq.com>
2016-11-14 22:47:32 +0800

    三子棋头文件

---
+++ checkerboard.h
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define ROW 3
+#define COLU 3
+void init(char a[ROW][COLU], int x, int y);
+void print(char a[ROW][COLU]);
+void play(char a[ROW][COLU], int x, int y);
+void computer(char a[ROW][COLU], int x, int y);
+void game(char a[ROW][COLU], int x, int y);
